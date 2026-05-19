68abf310f40da1b4f1753898e0ca6b7c3ba5ec1b
nofyrabe777 <nofyrabe@gmail.com>
2026-05-19 12:41:55 +0300

    Sauvegarde

---
+++ ToDo.md
@@ -0,0 +1,9 @@
+#### Preparation :
+[OK] Creation de depot
+[OK] Creation de l'environnement Python
+
+#### Creation et idition des fichier :
+[] Decodeur
+    [] 
+[] main
+    []
---
+++ decodeur.py
@@ -0,0 +1,6 @@
+print ("Bienvenue dans le décodeur de messages secrets !")
+print ("Veuillez entrer le message codé que vous souhaitez décoder :")
+mesage_a_decoder = input()
+
+def decoder_message(message_a_decoder):
+    message_a_decoder =
